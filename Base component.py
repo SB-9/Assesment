fea1c8c4ee561d3170ec1d0eff0d5d2bbfe79aa8
SB-9 <burgesss@middleton.school.nz>
2022-05-10 14:54:10 +1200

    Update Base component.py

--- Base component.py
+++ Base component.py
@@ -1,1 +1,13 @@
 """Base component for Maori quiz made by Samuel Burgess. Created on 9th March 2022."""
+
+
+def welcome():
+    print("Welcome to my Maori quiz!")
+    name = input("What is your name?: ")
+    while len(name) <= 1:
+        name = input("please enter a name longer than 1 character: ")
+    print(f"Welcome {name}, I hope you have a fun time playing my Quiz!")
+
+
+# Main routine
+welcome()
