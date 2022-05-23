6787515de77895169f7ca3d4516a016b27539cd4
SB-9 <burgesss@middleton.school.nz>
2022-05-23 20:15:17 +1200

    Create first trial and start second trial of questions

---
+++ Scoring_trial_1.py
@@ -0,0 +1,13 @@
+"""First trial of the scoring component for Maori quiz, made by Samuel Burgess"""
+
+answers = ["tahi", "rua", "toru", "wha", "rima", "ono", "whitu", "waru", "iwa", "tekau"]
+questions = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+num = 0
+
+for item in answers:
+    user_answer = input(f"what is the Maori name for the number {questions[num]}?: ")
+    if user_answer.lower() == answers[num]:
+        print("correct")
+    else:
+        print("incorrect")
+    num += 1
