73c6fc12200639a36f6650a33259e9703bf0bd1b
SB-9 <burgesss@middleton.school.nz>
2022-05-23 20:27:42 +1200

    Update Questions_trial_2.py

--- Questions_trial_2.py
+++ Questions_trial_2.py
@@ -1,13 +1,64 @@
 """Second trial of the question component for Maori quiz, made by Samuel Burgess"""
 
-answers = ["tahi", "rua", "toru", "wha", "rima", "ono", "whitu", "waru", "iwa", "tekau"]
-questions = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
-num = 0
+answer_1 = "tahi"
+answer_2 = "rua"
+answer_3 = "toru"
+answer_4 = "wha"
+answer_5 = "rima"
+answer_6 = "ono"
+answer_7 = "whitu"
+answer_8 = "waru"
+answer_9 = "iwa"
+answer_10 = "tekau"
 
-for item in answers:
-    user_answer = input(f"what is the Maori name for the number {questions[num]}?: ")
-    if user_answer.lower() == answers[num]:
-        print("correct")
-    else:
-        print("incorrect")
-    num += 1
+user_answer = input("What is the Maori name for the number one?: ")
+if answer_1 == user_answer:
+    print("correct")
+else:
+    print("incorrect")
+user_answer = input("What is the Maori name for the number two?: ")
+if answer_2 == user_answer:
+    print("correct")
+else:
+    print("incorrect")
+user_answer = input("What is the Maori name for the number three?: ")
+if answer_3 == user_answer:
+    print("correct")
+else:
+    print("incorrect")
+user_answer = input("What is the Maori name for the number four?: ")
+if answer_4 == user_answer:
+    print("correct")
+else:
+    print("incorrect")
+user_answer = input("What is the Maori name for the number five?: ")
+if answer_5 == user_answer:
+    print("correct")
+else:
+    print("incorrect")
+user_answer = input("What is the Maori name for the number six?: ")
+if answer_6 == user_answer:
+    print("correct")
+else:
+    print("incorrect")
+user_answer = input("What is the Maori name for the number seven?: ")
+if answer_7 == user_answer:
+    print("correct")
+else:
+    print("incorrect")
+user_answer = input("What is the Maori name for the number eight?: ")
+if answer_8 == user_answer:
+    print("correct")
+else:
+    print("incorrect")
+user_answer = input("What is the Maori name for the number nine?: ")
+if answer_9 == user_answer:
+    print("correct")
+else:
+    print("incorrect")
+user_answer = input("What is the Maori name for the number ten?: ")
+if answer_10 == user_answer:
+    print("correct")
+else:
+    print("incorrect")
+
