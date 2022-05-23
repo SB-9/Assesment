6cb01e5c5f990a90bd116bafb40b9f712ed0300a
SB-9 <burgesss@middleton.school.nz>
2022-05-24 08:37:56 +1200

    Create Looping_v1.py

---
+++ Looping_v1.py
@@ -0,0 +1,34 @@
+""""Looping component version 1 made by samuel burgess for Maori quiz"""
+
+
+# functions go here
+def question():
+    # Setup answers
+    answers = ["tahi", "rua", "toru", "wha", "rima", "ono", "whitu", "waru", "iwa", "tekau"]
+    # Setup question numbers
+    questions = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+    # Setup round counter
+    round_ = 0
+
+    # Loop until ten questions are done
+    for item in answers:
+        # get user input
+        user_answer = input(f"what is the Maori name for the number {questions[round_]}?: ")
+        # if correct tell the user
+        if user_answer.lower() == answers[round_]:
+            print("correct")
+        # else tell the user incorrect
+        else:
+            print("incorrect")
+        # add one to the round counter
+        round_ += 1
+
+
+# Setup variables
+playing = "l"
+
+# Man routine go here
+while playing.lower() != "x":
+    question()
+    playing = input("Do you want to play again? (x to quit any other key to play again):  ")
+
