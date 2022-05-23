6eee6a21d37ec0db6586aeba44e3ceabb6276a8e
SB-9 <burgesss@middleton.school.nz>
2022-05-23 21:03:29 +1200

    Create Questions_function.py

---
+++ Questions_function.py
@@ -0,0 +1,28 @@
+"""second version of the question component for Maori quiz, made by Samuel Burgess"""
+
+
+# variables go here
+def question():
+    # Setup answers
+    answers = ["tahi", "rua", "toru", "wha", "rima", "ono", "whitu", "waru", "iwa", "tekau"]
+    # Setup question numbers
+    questions = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+    # Setup round counter
+    round_ = 0
+
+    # Loop until ten questions are done
+    for item in answers:
+        # get user input
+        user_answer = input(f"what is the Maori name for the number {questions[round_]}?: ")
+        # if correct tell the user
+        if user_answer.lower() == answers[round_]:
+            print("correct")
+        # else tell the user incorrect
+        else:
+            print("incorrect")
+        # add one to the round counter
+        round_ += 1
+
+
+# main routine goes here
+question()
